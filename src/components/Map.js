ca375582380d4da7cad866aee07f0bb4606c020b
young-d <dodoga096@gmail.com>
2022-01-25 06:12:30 +0900

    feat: 지도 클릭 시 마커생성 이벤트 구현

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Marker, NaverMap } from 'react-naver-maps';
 
-const Map = () => {
+const Map = ({ isLoading = false }) => {
   const jibun = '잠실동 312-4';
   const price = '28억';
+  const [currentLocation, setCurrentLocation] = useState({
+    lat: 37.506608542,
+    lng: 127.065369677,
+  });
+
   const markerIcon = {
-    content: `<div class="marker">
-                <div class="marker__asset-info">
-                  <div class="marker__asset-info__jibun">${jibun}</div>
-                  <span class="marker__asset-info__price">${price}</span>
-                </div>
-              </div>`,
+    content: `
+    <div class="marker ${isLoading ? 'loading' : ''}">
+        ${
+          !isLoading
+            ? `<div class="marker__asset-info">
+              <div class="marker__asset-info__jibun">${jibun}</div>
+              <span class="marker__asset-info__price">${price}</span>
+            </div>`
+            : ''
+        }
+      </div>
+    `,
     size: new window.naver.maps.Size(16, 16),
-    anchor: new window.naver.maps.Point(16, 16),
+  };
+
+  const handleClickMap = e => {
+    setCurrentLocation({ lat: e.coord.y, lng: e.coord.x });
   };
 
   return (
     <>
       <NaverMap
         mapDivId={'react-naver-map'}
+        ncp
         style={{
           width: '100vw',
           height: '100vh',
-        }}>
-        <Marker
-          position={{ lat: 37.3595704, lng: 127.105399 }}
-          icon={markerIcon}
-        />
+        }}
+        center={{ ...currentLocation }}
+        zoom={16}
+        onClick={handleClickMap}>
+        <Marker position={currentLocation} icon={markerIcon} />
       </NaverMap>
     </>
   );

0c77ad0691948b3fa6daef1e15b088275f804dcf
young-d <dodoga096@gmail.com>
2022-01-24 18:38:54 +0900

    init: 프로젝트 초기 개발환경 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80, 
+  tabWidth: 2, 
+  singleQuote: true, 
+  semi: true, 
+  arrowParens: 'always', 
+  endOfLine: 'auto',
+  bracketSpacing: true, 
+  jsxBracketSameLine: true, 
+}

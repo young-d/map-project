8325ac19f61d6e85a7f1774915139fa194a19547
young-d <dodoga096@gmail.com>
2022-01-25 20:25:31 +0900

    fix: 현재 url이 루트이고 지도 영역 최초 클릭시에만 default asset_pnu 라우팅하도록 수정

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import { Marker, NaverMap } from 'react-naver-maps';
+import styled from '@emotion/styled';
 
 const DEFAULT_LAT = 37.506608542;
 const DEFAULT_LNG = 127.065369677;
@@ -35,23 +36,30 @@ const Map = ({ assetDetail, isLoading = false, onClick, markInit = false }) => {
   };
 
   return (
-    <NaverMap
-      mapDivId={'react-naver-map'}
-      ncp
-      style={{
-        width: '100vw',
-        height: '100vh',
-      }}
-      center={{ ...currentPointer }}
-      defaultZoom={DEFAULT_ZOOM}
-      zoomOrigin={{ lng: currentPointer.lng, lat: currentPointer.lat }}
-      onClick={e => onClick(e)}
-      onZoomChanged={zoom => setZoom(() => zoom)}>
-      {markInit && zoom >= DEFAULT_ZOOM && (
-        <Marker position={currentPointer} icon={markerIcon} />
-      )}
-    </NaverMap>
+    <Wrapper>
+      <NaverMap
+        mapDivId={'react-naver-map'}
+        ncp
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        center={{ ...currentPointer }}
+        defaultZoom={DEFAULT_ZOOM}
+        zoomOrigin={{ lng: currentPointer.lng, lat: currentPointer.lat }}
+        onClick={e => onClick(e)}
+        onZoomChanged={zoom => setZoom(() => zoom)}>
+        {markInit && zoom >= DEFAULT_ZOOM && (
+          <Marker position={currentPointer} icon={markerIcon} />
+        )}
+      </NaverMap>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  width: 86%;
+  height: 100%;
+`;
 
 export default Map;
